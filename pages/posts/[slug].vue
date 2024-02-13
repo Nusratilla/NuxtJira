01a4f96fe0ac4c2e476365356cf1b52717e3df53
Nusrat <nusratilla.88@gmail.com>
2024-02-13 23:03:00 +0500

    useRoute

--- pages/posts/[slug].vue
+++ pages/posts/[slug].vue
@@ -1,12 +1,13 @@
 <script lang="ts" setup>
 const doc = 'Documentation'
+const route = useRoute()
 </script>
 
 <template>
   <div>
     <div>
       <h1>
-        {{ doc }} id
+        {{ doc }} id: 17.55 {{ route.params.slug }}
       </h1>
     </div>
   </div>
