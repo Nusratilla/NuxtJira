46b1dcabc09dfba9998b5e1cd98c906c6646f85e
Nusrat <nusratilla.88@gmail.com>
2024-02-13 22:53:56 +0500

    id qoshish

---
+++ pages/posts/[id].vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const doc = 'Documentation'
+</script>
+
+<template>
+  <div>
+    <div>
+      <h1>
+        {{ doc }} id
+      </h1>
+    </div>
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped></style>
